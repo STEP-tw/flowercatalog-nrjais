403fc4bfac2c0590350f6ca8b5bdd50843a3d11d
Neeraj Jaiswal <neerajj85@gmail.com>
2018-01-09 17:40:04 +0530

    Comment form is hidden when not logged in and name is shown in place of name input

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const http = require('http');
 const fs = require('fs');
 const serveStaticFile = require('./file.js').serveStaticFile;
+const timeStamp = require('./time.js').timeStamp;
 
 const webapp = require('./webapp');
 
-let users = [{ user: 'nrjais' }, { user: 'arvind' }, { user: 'debuc' }, { user: 'ravinder' }];
+let commentForm = `<a href="/logout">Logout</a>
+<h2>Leave a comment</h2>
+<form class="input" action = "/addcomment" method = "post" >
+Name : <b>NAME_USER</b>
+<br>
+<br> Comment :
+<textarea name="comment" rows="4" cols="40"></textarea>
+<br>
+<br>
+<button type="submit">Submit</button>
+</form>`;
+
+let loginButton = `<h2>Login to comment</h2><a href="/login.html">Login</a>`;
+
+let users = [{ user: 'nrjais', name:"Neeraj Jaiswal"}];
 let PORT = 9999;
 
 let comments = fs.readFileSync('./data/data.json', 'utf8');
@@ -13,23 +28,24 @@ comments = JSON.parse(comments);
 let app = webapp();
 
 const saveComment = function (req, res) {
-  if (isNotValidUser(req)) {
+  if (!isUserLoggedIn(req)) {
     res.redirect('/login.html');
     return;
   }
   let data = req.body;
-  if (!data.name || !data.comment) {
+  if (!data.comment) {
     res.redirect('/guestbook.html');
     return;
   }
+  data.name = req.user.name; 
   data.time = new Date().toLocaleString();
   comments.unshift(data);
   saveCommentsData();
   res.redirect('guestbook.html');
 };
 
-const isNotValidUser = function (req) {
-  return !(req.user && req.cookies.sessionId == req.user.sessionId);
+const isUserLoggedIn = function (req) {
+  return req.user && req.cookies.sessionId == req.user.sessionId;
 }
 
 const saveCommentsData = function () {
@@ -42,13 +58,19 @@ const logError = function (err) {
     console.log(err);
 };
 
-const logRequest = function (req, res) {
-  const time = new Date().toLocaleString();
-  console.log(`${time} ${req.method} ${req.url}`);
-};
+let logRequest = (req, res) => {
+  let text = ['--------------------------------',
+    `${timeStamp()}`,
+    `${req.method} ${req.url}`,
+    `HEADERS=> ${JSON.stringify((req.headers))}`,
+    `COOKIES=> ${JSON.stringify((req.cookies))}`,
+    `BODY=> ${JSON.stringify((req.body))}`, ''].join('\n');
+  fs.appendFile('request.log', text, () => { });
+  console.log(`${req.method} ${req.url}`);
+}
 
 const sendCommentData = function (req, res) {
-  res.setHeader('content-type', 'text/javascript');
+  res.setHeader('content-type', 'application/json');
   let commentsData = JSON.stringify(comments);
   res.write(commentsData);
   res.end();
@@ -85,10 +107,25 @@ const logoutUser = function (req, res) {
   res.redirect('/index.html');
 }
 
+const serveGuestbook = function (req, res) {
+  let page = fs.readFileSync('public/guestbook.html', 'utf8')
+  if (isUserLoggedIn(req)) {
+    let userName = req.user.name;
+    commentFormUser = commentForm.replace('NAME_USER', userName);
+    page = page.replace('COMMENTFORM', commentFormUser);
+  } else {
+    page = page.replace('COMMENTFORM', loginButton);
+  }
+  res.setHeader('content-type','text/html');
+  res.write(page);
+  res.end();
+}
+
 app.use(logRequest);
 app.use(loadSession);
 app.get('/', (req, res) => res.redirect('index.html'));
 app.post('/addcomment', saveComment);
+app.get('/guestbook.html', serveGuestbook);
 app.get('/data', sendCommentData);
 app.post('/login', loginUser);
 app.get('/logout', logoutUser);
